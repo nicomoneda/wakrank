29df4fd41cf583eb4264a7349802beb8495a4334
nicomoneda <nico.braun@free.fr>
2024-02-23 16:13:43 +0100

    Improve dungeon index view and controller

--- app/controllers/dungeons_controller.rb
+++ app/controllers/dungeons_controller.rb
@@ -1,29 +1,37 @@
 class DungeonsController < ApplicationController
     def index
         @dungeons = Dungeon.all
+
         @coffres = []
+        @characters = []
+        @rankedCharacters = []
         @dungeons.each do |dungeon|
-            sum = 0
+
             all_ranking = Ranking.where(dungeon: dungeon)
-            puts all_ranking
-            all_ranking.each do |rank|
-                puts rank
-                sum += rank.stasis
-            end
-            @coffres << sum
+            totalCharacters = Character.where("level >= #{dungeon.modulation - 14}").count
+            
+            @coffres << all_ranking.sum(:stasis)
+            @rankedCharacters << all_ranking.count 
+            @characters << totalCharacters
+            # @characters.each do |character|
+            #     Ranking.where(character: character, dungeon: dungeon).exists? ? nil : sumCharacters += 1 # EH OUI. On peut pas ++ dans Ruby L.U.L
+            # end
+            # @noRankCharacters << sumCharacters
+
+            # sumCoffres = 0
         end
-        puts "Tableau coffres = #{@coffres}"
     end
 
     def show
         @dungeon = Dungeon.find(params[:id])
         @ranking = Ranking.new
+        
         @characters = Character.where("level >= #{@dungeon.modulation - 14}")
         @noRankCharacters = []
         @characters.each do |character|
             Ranking.where(character: character, dungeon: @dungeon).exists? ? nil : @noRankCharacters << character
         end
-        puts "Persos non classés : #{@noRankCharacters}"
+
         @ranking_dungeon = @dungeon.rankings
         @total_coffres = 0
         @ranking_dungeon.each do |rank|
