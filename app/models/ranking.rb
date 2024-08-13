4f10c27f31c658a6c185cd28917febdd5da47694
nicomoneda <nico.braun@free.fr>
2024-08-13 15:13:23 +0200

    Correct character and ranking models

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -2,9 +2,14 @@ class Ranking < ApplicationRecord
   belongs_to :character
   belongs_to :dungeon
 
-  # validates :stasis, numericality: { only_integer: true }
-  # validates :rank, numericality: { only_integer: true }
-  
-  # validates :stasis, inclusion: { in: (1..10) }
-  # validates :rank, numericality: { greater_than_or_equal_to: 1 }
+  validates :stasis, numericality: { only_integer: true }, unless: :isBreach?
+  validates :stasis, inclusion: { in: (1..10) }, unless: :isBreach?
+
+  validates :rank, numericality: { only_integer: true }, if: :isBreach?
+  validates :rank, numericality: { greater_than_or_equal_to: 1 }, if: :isBreach?
+
+  private
+  def isBreach?
+    return if (dungeon.kind.name == "Brèche Dimensionnelle" || dungeon.kind.name == "Brèche Dimensionnelle Ultime" || dungeon.kind.name == "Boss Ultime à Score")
+  end
 end
