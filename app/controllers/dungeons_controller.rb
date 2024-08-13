ed26c096f245d5c6612ae7548c9164fec1c37509
nicomoneda <nico.braun@free.fr>
2024-08-13 15:12:51 +0200

    Correct characters displayed

--- app/controllers/dungeons_controller.rb
+++ app/controllers/dungeons_controller.rb
@@ -3,16 +3,21 @@ class DungeonsController < ApplicationController
         @dungeons = Dungeon.all
 
         @coffres = []
-        @characters = []
+        @allCharacters = []
         @rankedCharacters = []
+
         @dungeons.each do |dungeon|
 
-            all_ranking = Ranking.where(dungeon: dungeon)
-            totalCharacters = Character.where("level >= #{dungeon.modulation - 14}").count
-            
+            all_ranking = Ranking.where(dungeon: dungeon, user_id: current_user.id)
+            totalCharacters = Character.where("level >= #{dungeon.modulation - 14}").where(user_id: current_user.id)
+            puts "CURRENT_USER ID:"
+            puts current_user.id
+            puts "CHARACTER"
+            puts totalCharacters
+
             @coffres << all_ranking.sum(:stasis)
             @rankedCharacters << all_ranking.count 
-            @characters << totalCharacters
+            @allCharacters << totalCharacters.count
             # @characters.each do |character|
             #     Ranking.where(character: character, dungeon: dungeon).exists? ? nil : sumCharacters += 1 # EH OUI. On peut pas ++ dans Ruby L.U.L
             # end
@@ -26,10 +31,10 @@ class DungeonsController < ApplicationController
         @dungeon = Dungeon.find(params[:id])
         @ranking = Ranking.new
         
-        @characters = Character.where("level >= #{@dungeon.modulation - 14}")
+        @characters = Character.where("level >= #{@dungeon.modulation - 14}").where(user_id: current_user.id)
         @noRankCharacters = []
         @characters.each do |character|
-            Ranking.where(character: character, dungeon: @dungeon).exists? ? nil : @noRankCharacters << character
+            Ranking.where(character: character, dungeon: @dungeon, user_id: current_user.id).exists? ? nil : @noRankCharacters << character
         end
 
         @ranking_dungeon = @dungeon.rankings
